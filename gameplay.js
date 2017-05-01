f1f7223f174b80f0dc1ec19c9d53cccfe32e6ef9
Raúl Villares <raulvillares@gmail.com>
2017-05-02 00:19:06 +0200

    Square and Board prototypes created

--- gameplay.js
+++ gameplay.js
@@ -1,32 +1,62 @@
 const NUMBER_CHANGEABLE_IMAGES = 3;
+const IMAGES_FOLDER = "./img/";
+const IMAGES_EXTENSION = ".png";
 
 const EMPTY = 0;
 const TIC = 1;
 const TAC = 2;
 const FIXED_EMPTY = 3;
-const FIXEX_TIC = 4;
+const FIXED_TIC = 4;
 const FIXED_TAC = 5;
 
-const images = ["empty", "blue", "pink", "box", "blue_fixe", "pink_fixed"];
-var board = [];
+const images = ["empty", "blue", "pink", "box", "blueFixed", "pinkFixed"];
 
 function Square(initialImage) {
     this.currentImage = initialImage;
+    if(initialImage < NUMBER_CHANGEABLE_IMAGES)
+        this.changeable = true; 
+    else 
+        this.changeable = false;  
 }
 
 Square.prototype.change = function() {
-    this.currentImage = (this.currentImage+1) % NUMBER_CHANGEABLE_IMAGES;
+    if(this.changeable)
+        this.currentImage = (this.currentImage+1) % NUMBER_CHANGEABLE_IMAGES;
 }
 
 Square.prototype.toString = function() {
     return images[this.currentImage];
 }
 
-var square1 = new Square(0);
-console.log(square1.toString());
-square1.change();
-console.log(square1.toString());
-square1.change();
-console.log(square1.toString());
-square1.change();
-console.log(square1.toString());
+function Board() {
+    this.squares = [];
+    this.squares.push([new Square(EMPTY), new Square(EMPTY), new Square(EMPTY), new Square(EMPTY), new Square(EMPTY)]);
+    this.squares.push([new Square(EMPTY), new Square(EMPTY), new Square(EMPTY), new Square(FIXED_TIC), new Square(EMPTY)]);
+    this.squares.push([new Square(EMPTY), new Square(EMPTY), new Square(EMPTY), new Square(EMPTY), new Square(EMPTY)]);
+    this.squares.push([new Square(EMPTY), new Square(FIXED_TIC), new Square(FIXED_EMPTY), new Square(0), new Square(FIXED_TAC)]);
+    this.squares.push([new Square(EMPTY), new Square(EMPTY), new Square(EMPTY), new Square(EMPTY), new Square(FIXED_TAC)]);
+}
+
+Board.prototype.createElement = function() {
+    var elementBoard = document.createElement("div");
+    elementBoard.className = "board";
+    this.squares.forEach(function(row) {
+        var elementRow = document.createElement("div");
+        elementRow.className = "row";
+        elementBoard.appendChild(elementRow);
+        row.forEach(function(square) {
+            var elementSquare = document.createElement("div");
+            elementSquare.className = "square";
+            var elementSquareImage = document.createElement("img");
+            elementSquareImage.className = "squareImage";
+            elementSquareImage.src = IMAGES_FOLDER+images[square.currentImage]+IMAGES_EXTENSION;
+            elementSquare.appendChild(elementSquareImage);
+            elementRow.appendChild(elementSquare);
+        });
+    });
+    return elementBoard;
+}
+
+var board = new Board();
+console.log(board);
+console.log(board.createElement());
