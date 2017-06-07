081dea171e6d8c8dca26a3b21ed9fc56b2af5922
Matthew Hill <matthew@linuxbox.linuxbox>
2017-06-07 15:13:03 -0400

    Added levels 8-19 from provided source

--- js/levels.js
+++ js/levels.js
@@ -12,7 +12,7 @@ define(['js/properties'], function(properties) {
         "blueFixed",
         "pinkFixed"
         ],
-    "initialSquares": 
+    "initialSquares":
         [
             [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
             [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY],
@@ -34,7 +34,7 @@ define(['js/properties'], function(properties) {
         "yellowFixed",
         "greenFixed"
         ],
-    "initialSquares": 
+    "initialSquares":
         [
             [properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.FIXED_TIC],
             [properties.FIXED_TAC, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
@@ -47,7 +47,7 @@ define(['js/properties'], function(properties) {
     const level3 = {
     "levelNumber" : 3,
     "backgroundImage" : "uncolored_castle",
-    "imagesFilesNames" : 
+    "imagesFilesNames" :
         [
         "empty",
         "ghost",
@@ -97,8 +97,8 @@ define(['js/properties'], function(properties) {
         "minty",
         "paleyellow",
         "brick",
-        "mintyfixed",
-        "paleyellowfixed"
+        "mintyFixed",
+        "paleyellowFixed"
     ],
     "initialSquares" :
     [
@@ -155,7 +155,294 @@ define(['js/properties'], function(properties) {
     ]
     };
 
-    const levels = [level1, level2, level3, level4, level5, level6, level7];
+    const level8 = {
+      "levelNumber" : 8,
+      "backgroundImage": "colored_shroom",
+      "imagesFilesNames":
+      [
+        "empty",
+        "orange",
+        "minty",
+        "wood",
+        "orangeFixed",
+        "mintyFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_TAC, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY,  properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.FIXED_TAC, properties.EMPTY, properties.FIXED_TAC]
+      ]
+    };
+
+    const level9 = {
+      "levelNumber" : 9,
+      "backgroundImage": "uncolored_peaks",
+      "imagesFilesNames":
+      [
+        "empty",
+        "red",
+        "paleyellow",
+        "stone",
+        "redFixed",
+        "paleyellowFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC],
+        [properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.FIXED_TIC, properties.FIXED_EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY,  properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.FIXED_TAC]
+      ]
+    };
+
+    const level10 = {
+      "levelNumber" : 10,
+      "backgroundImage": "blue_land",
+      "imagesFilesNames":
+      [
+        "empty",
+        "lime",
+        "ghost",
+        "metal",
+        "limeFixed",
+        "ghostFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY],
+        [properties.FIXED_TAC, properties.FIXED_EMPTY, properties.FIXED_TAC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY,  properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC]
+      ]
+    };
+
+    const level11 = {
+      "levelNumber" : 11,
+      "backgroundImage": "blue_shroom",
+      "imagesFilesNames":
+      [
+        "empty",
+        "purple",
+        "blue",
+        "brick",
+        "purpleFixed",
+        "blueFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC,  properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY]
+      ]
+    };
+
+    const level12 = {
+      "levelNumber" : 12,
+      "backgroundImage": "colored_talltrees",
+      "imagesFilesNames":
+      [
+        "empty",
+        "pink2",
+        "pistachio",
+        "metal",
+        "pink2Fixed",
+        "pistachioFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.FIXED_TAC],
+        [properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_EMPTY, properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY,  properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.EMPTY],
+        [properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.FIXED_EMPTY, properties.EMPTY]
+      ]
+    };
+
+    const level13 = {
+      "levelNumber" : 13,
+      "backgroundImage": "uncolored_plain",
+      "imagesFilesNames":
+      [
+        "empty",
+        "canvasBlue",
+        "greyblue",
+        "castle",
+        "canvasBlueFixed",
+        "greyblueFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.FIXED_EMPTY],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_TAC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY,  properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY]
+      ]
+    };
+
+    const level14 = {
+      "levelNumber" : 14,
+      "backgroundImage": "colored_forest",
+      "imagesFilesNames":
+      [
+        "empty",
+        "blackandwhite",
+        "orange",
+        "wood",
+        "blackandwhiteFixed",
+        "orangeFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TIC],
+        [properties.EMPTY, properties.FIXED_TIC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY,  properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY]
+      ]
+    };
+
+    const level15 = {
+      "levelNumber" : 15,
+      "backgroundImage": "colored_piramids",
+      "imagesFilesNames":
+      [
+        "empty",
+        "ghost",
+        "green",
+        "brick",
+        "ghostFixed",
+        "greenFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC],
+        [properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TAC, properties.EMPTY],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC]
+      ]
+    };
+
+    const level16 = {
+      "levelNumber" : 16,
+      "backgroundImage": "uncolored_hills",
+      "imagesFilesNames":
+      [
+        "empty",
+        "rainbow",
+        "yellow",
+        "stone",
+        "rainbowFixed",
+        "yellowFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY],
+        [properties.FIXED_TAC, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY,  properties.FIXED_EMPTY, properties.FIXED_TIC, properties.EMPTY],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TIC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TAC, properties.FIXED_TIC],
+        [properties.EMPTY, properties.FIXED_TIC, properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY]
+      ]
+    };
+    const level17 = {
+      "levelNumber" : 17,
+      "backgroundImage": "uncolored_talltrees",
+      "imagesFilesNames":
+      [
+        "empty",
+        "garnet",
+        "greyblue",
+        "metal",
+        "garnetFixed",
+        "greyblueFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.FIXED_TAC, properties.FIXED_TAC, properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TAC],
+        [properties.FIXED_TAC, properties.FIXED_EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY,  properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY],
+        [properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY],
+        [properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY]
+      ]
+    };
+
+    const level18 = {
+      "levelNumber" : 18,
+      "backgroundImage": "colored_desert",
+      "imagesFilesNames":
+      [
+        "empty",
+        "red",
+        "lime",
+        "metal2",
+        "redFixed",
+        "limeFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY,  properties.FIXED_EMPTY, properties.FIXED_TIC, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TIC],
+        [properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.FIXED_TIC, properties.EMPTY]
+      ]
+    };
+
+    const level19 = {
+      "levelNumber" : 19,
+      "backgroundImage": "colored_castle",
+      "imagesFilesNames":
+      [
+        "empty",
+        "pink2",
+        "ghost",
+        "wood",
+        "pink2Fixed",
+        "ghostFixed"
+      ],
+      "initialSquares":
+      [
+        [properties.EMPTY, properties.FIXED_TIC, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC],
+        [properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.FIXED_EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY,  properties.EMPTY, properties.EMPTY, properties.FIXED_TAC],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.FIXED_TAC, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY],
+        [properties.EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC, properties.FIXED_EMPTY, properties.EMPTY, properties.EMPTY, properties.FIXED_TIC]
+      ]
+    };
+
+    const levels = [level1, level2, level3, level4, level5, level6, level7, level8, level9, level10, level11, level12, level13, level14, level15, level16, level17, level18, level19];
 
     return {
         getLevelData: function(levelNumber) {
