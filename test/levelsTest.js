98e1f94892223a10bbf5c41a47a16e4c03cc2065
LeonSinclair <lsinclai@tcd.ie>
2017-10-15 10:09:13 +0100

    Fixing minor Codacy issues.

--- test/levelsTest.js
+++ test/levelsTest.js
@@ -1,9 +1,9 @@
-import test from 'tape';
+import test from "tape";
 const requirejs = require("requirejs");
 
 test("levels.js tests", (assert) => {
     requirejs(["../require-config", "../js/levels"], function(config, levels) {
-        assert.notEqual(undefined, levels);
+        assert.notEqual(null, levels);
         assert.end();
     });
 });
