1133a614a0c5267c9172188101619b60cb8c99d1
Ali Zahir <alizahir@live.com>
2019-01-22 16:00:17 +0500

    added a new test file for sound.js

---
+++ test/soundTest.js
@@ -0,0 +1,28 @@
+const test = require('tape');
+const requirejs = require("requirejs");
+var player = require('play-sound')(opts = {});
+
+test("sound.js Varaible tests", (assert) => {
+    requirejs(["../require-config.js", "../js/sound.js"], function(config, sound) {
+        //player.play(sound.play().audio, (err) => {if (err) throw err});
+        assert.notEqual(null, sound);
+        assert.true(sound.SOUNDS.map(element => typeof(element) === 'string'), 'Every element in the array should be a string');
+        assert.is(3, sound.FORBIDDEN, 'Forbidden should be a value of 3');
+        assert.is(4, sound.PINNED, 'Pinned should be a value of 4');
+        assert.is(5, sound.UNPINNED, 'Unpinned should be a value of 5');
+        assert.is(6, sound.UNDO, 'Undo hould be a value of 6');
+        assert.is(7, sound.COMPLETED, 'Completed should be a value of 7');
+        assert.notEqual(null, sound.play);
+        assert.end();
+    });
+});
+
+test("sound.js audio file address tests", (assert) => {
+    requirejs(["../require-config.js", "../js/properties", "../js/sound.js"], function(config, properties, sound) {
+        for (let i = 0; i < sound.SOUNDS.length; i++) {
+          let address = '../' + properties.SOUNDS_FOLDER + sound.SOUNDS[i] + properties.SOUNDS_EXTENSION;
+          assert.is('string', typeof(address), 'Address should be a string');
+        }
+        assert.end();
+    });
+});
