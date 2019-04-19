28491951d9538c4006b96f2636ff49432c5b976a
iankameron <ian.kameron@gmail.com>
2019-04-19 10:50:58 +0900

    added test to check for integrity of level definitions

--- test/levelsTest.js
+++ test/levelsTest.js
@@ -3,7 +3,30 @@ const requirejs = require("requirejs");
 
 test("levels.js tests", (assert) => {
     requirejs(["./require-config.js", "./js/levels.js"], function(config, levels) {
+        console.log(levels);
         assert.notEqual(null, levels);
+        let levelCounter = 1;
+        while (levels.getLevelData(levelCounter)) {
+            const currentLevel = levels.getLevelData(levelCounter);
+            assert.equal("number", typeof currentLevel.levelNumber);
+            assert.equal("string", typeof currentLevel.backgroundImage);
+            assert.equal(6, currentLevel.imagesFilesNames.length);
+            let rowLengthCheck = true;
+            let rowLength = null;
+            for (let boardRow of currentLevel.initialSquares) {
+                if (rowLength === null) {
+                    rowLength = boardRow.length;
+                } else {
+                    if (boardRow.length !== rowLength) {
+                        rowLengthCheck = false;
+                    }
+                }
+            }
+            assert.equal(true, rowLengthCheck);  
+            assert.equal("number", typeof currentLevel.maxTimeAchievement);
+            assert.equal("number", typeof currentLevel.maxMovementsAchievement);
+            levelCounter ++;
+        }
         assert.end();
     });
 });
