177b5b5a48720932abdad7cac116fc31ed9b5b0e
Ian Cameron and Johannes Jarbratt <ian.kameron+johannes.jarbratt+github@gmail.com>
2019-04-26 10:55:55 +0900

    fleshed out level tests

--- test/levelsTest.js
+++ test/levelsTest.js
@@ -2,31 +2,59 @@ import test from "tape";
 const requirejs = require("requirejs");
 
 test("levels.js tests", (assert) => {
-    requirejs(["./require-config.js", "./js/levels.js"], function(config, levels) {
-        console.log(levels);
-        assert.notEqual(null, levels);
-        let levelCounter = 1;
-        while (levels.getLevelData(levelCounter)) {
-            const currentLevel = levels.getLevelData(levelCounter);
-            assert.equal("number", typeof currentLevel.levelNumber);
-            assert.equal("string", typeof currentLevel.backgroundImage);
-            assert.equal(6, currentLevel.imagesFilesNames.length);
-            let rowLengthCheck = true;
-            let rowLength = null;
-            for (let boardRow of currentLevel.initialSquares) {
-                if (rowLength === null) {
-                    rowLength = boardRow.length;
-                } else {
-                    if (boardRow.length !== rowLength) {
-                        rowLengthCheck = false;
-                    }
-                }
+  requirejs(["./require-config.js", "./js/levels.js"], function(
+    config,
+    levels
+  ) {
+    assert.notEqual(null, levels);
+    let levelCounter = 1;
+    while (levels.getLevelData(levelCounter)) {
+      const currentLevel = levels.getLevelData(levelCounter);
+
+      // levelNumber
+      assert.ok(currentLevel.hasOwnProperty("levelNumber"));
+      assert.equal("number", typeof currentLevel.levelNumber);
+
+      // backGroundImage
+      assert.ok(currentLevel.hasOwnProperty("backgroundImage"));
+      assert.equal("string", typeof currentLevel.backgroundImage);
+
+      // imagesFilesNames
+      assert.ok(currentLevel.hasOwnProperty("imagesFilesNames"));
+      assert.equal(6, currentLevel.imagesFilesNames.length);
+
+      // initialSquares
+      assert.ok(currentLevel.hasOwnProperty("initialSquares"));
+      assert.ok(Array.isArray(currentLevel.initialSquares));
+      let initialSquaresCheck = true;
+      let rowLength = null;
+      for (let boardRow of currentLevel.initialSquares) {
+        if (!Array.isArray(boardRow)) {
+          initialSquaresCheck = false;
+        } else {
+          // is an array
+          if (rowLength === null) {
+            // first row
+            rowLength = boardRow.length;
+          } else {
+            //subsequent rows
+            if (boardRow.length !== rowLength) {
+              initialSquaresCheck = false;
             }
-            assert.equal(true, rowLengthCheck);  
-            assert.equal("number", typeof currentLevel.maxTimeAchievement);
-            assert.equal("number", typeof currentLevel.maxMovementsAchievement);
-            levelCounter ++;
+          }
         }
-        assert.end();
-    });
+      }
+      assert.ok(initialSquaresCheck);
+      // maxTimeAchievement
+      assert.ok(currentLevel.hasOwnProperty("maxTimeAchievement"));
+      assert.equal("number", typeof currentLevel.maxTimeAchievement);
+
+      // maxMovementsAchievement
+      assert.ok(currentLevel.hasOwnProperty("maxMovementsAchievement"));
+      assert.equal("number", typeof currentLevel.maxMovementsAchievement);
+
+      levelCounter++;
+    }
+    assert.end();
+  });
 });
