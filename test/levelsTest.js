fb3d0169cc2285b6b2f4db2d14fb40fb16175483
Ian Cameron <ian.kameron@gmail.com>
2019-05-15 10:56:54 +0900

    finished level solver

--- test/levelsTest.js
+++ test/levelsTest.js
@@ -12,95 +12,97 @@ test("levels.js tests", (assert) => {
     assert.notEqual(null, levels);
     let levelCounter = 1;
 
-    const checkLevel = levels.getLevelData(1);
-    let startBoard = checkLevel.initialSquares;
+    while (levels.getLevelData(levelCounter)) {
+      const currentLevel = levels.getLevelData(levelCounter);
 
-    // transformedBoard[0][4] = 2;
-    // transformedBoard[1][4] = 2;
-    const testBoard = [
-      [1,0,0],
-      [1,3,1],
-      [2,2,0]];
-    console.log(levelSolveable(testBoard));
+      // levelNumber
+      assert.ok(currentLevel.hasOwnProperty("levelNumber"));
+      assert.equal(typeof currentLevel.levelNumber, "number");
 
-    // 
+      // backGroundImage
+      assert.ok(currentLevel.hasOwnProperty("backgroundImage"));
+      assert.equal(typeof currentLevel.backgroundImage, "string");
 
+      // imagesFilesNames
+      assert.ok(currentLevel.hasOwnProperty("imagesFilesNames"));
+      assert.equal(currentLevel.imagesFilesNames.length, 6);
 
-    // while (levels.getLevelData(levelCounter)) {
-    //   const currentLevel = levels.getLevelData(levelCounter);
+      // initialSquares
+      assert.ok(currentLevel.hasOwnProperty("initialSquares"));
+      assert.ok(Array.isArray(currentLevel.initialSquares));
+        // Check for regularity of board (all rows evenly sized)
+      let isRegular = isBoardRegular(currentLevel.initialSquares);
+      assert.ok(isRegular);
+        // If regular, check there's only one solution
+      if (isRegular) {
+        let solutions = getAllSolutions(currentLevel.initialSquares);
+        // Uncomment the following to sneak peek the solution
+        // console.log(solutions);
+        assert.equal(solutions.length, 1);
+      }
 
-    //   // levelNumber
-    //   assert.ok(currentLevel.hasOwnProperty("levelNumber"));
-    //   assert.equal("number", typeof currentLevel.levelNumber);
+      // maxTimeAchievement
+      assert.ok(currentLevel.hasOwnProperty("maxTimeAchievement"));
+      assert.equal(typeof currentLevel.maxTimeAchievement, "number");
 
-    //   // backGroundImage
-    //   assert.ok(currentLevel.hasOwnProperty("backgroundImage"));
-    //   assert.equal("string", typeof currentLevel.backgroundImage);
+      // maxMovementsAchievement
+      assert.ok(currentLevel.hasOwnProperty("maxMovementsAchievement"));
+      assert.equal(typeof currentLevel.maxMovementsAchievement,"number");
 
-    //   // imagesFilesNames
-    //   assert.ok(currentLevel.hasOwnProperty("imagesFilesNames"));
-    //   assert.equal(6, currentLevel.imagesFilesNames.length);
-
-    //   // initialSquares
-    //   assert.ok(currentLevel.hasOwnProperty("initialSquares"));
-    //   assert.ok(Array.isArray(currentLevel.initialSquares));
-    //   let initialSquaresCheck = true;
-    //   let rowLength = null;
-    //   for (let boardRow of currentLevel.initialSquares) {
-    //     if (!Array.isArray(boardRow)) {
-    //       initialSquaresCheck = false;
-    //     } else {
-    //       // is an array
-    //       if (rowLength === null) {
-    //         // first row
-    //         rowLength = boardRow.length;
-    //       } else {
-    //         //subsequent rows
-    //         if (boardRow.length !== rowLength) {
-    //           initialSquaresCheck = false;
-    //         }
-    //       }
-    //     }
-    //   }
-    //   assert.ok(initialSquaresCheck);
-    //   // maxTimeAchievement
-    //   assert.ok(currentLevel.hasOwnProperty("maxTimeAchievement"));
-    //   assert.equal("number", typeof currentLevel.maxTimeAchievement);
-
-    //   // maxMovementsAchievement
-    //   assert.ok(currentLevel.hasOwnProperty("maxMovementsAchievement"));
-    //   assert.equal("number", typeof currentLevel.maxMovementsAchievement);
-
-    //   levelCounter++;
-    // }
+      levelCounter++;
+    }
     assert.end();
   });
 });
 
-function levelSolveable (board) {
+function isBoardRegular (board) {
+  let isBoardRegular = true;
+  let rowLength = null;
+  for (let boardRow of board) {
+    if (!Array.isArray(boardRow)) {
+      isBoardRegular = false;
+    } else {
+      // is an array
+      if (rowLength === null) {
+        // first row
+        rowLength = boardRow.length;
+      } else {
+        //subsequent rows
+        if (boardRow.length !== rowLength) {
+          isBoardRegular = false;
+        }
+      }
+    }
+  }
+  return isBoardRegular;
+}
+
+function getAllSolutions (board) {
   // build board
   let {workBoard, emptyCount} = prepareBoard(board);
   const size = workBoard[0].length;
-  console.log(workBoard, emptyCount);
   let moves = [];
-  // let solutions = [];
-  let solveable = false; 
-// check count of empty cells (for check)
+  let solutions = [];
+  let deadEnds = [];
+
   const solveBoard = () => {
     let guess = {surrounding: 0};
     let i = 0;
     while (i < size * size && moves.length !== emptyCount) {
       let rownum = Math.floor(i / size);
       let colnum = i % size;
-      if (workBoard[rownum][colnum] === 0) { // only check if it hasn't been played on}
+      if (workBoard[rownum][colnum] === 0) { // only check if it hasn't been played on
         let {cantPlay, possibleGuess} = checkPlay(workBoard,rownum,colnum);
         if (cantPlay.length === 2) {
-          return false;
+          // console.log("DEAD END");
+          // tracks dead ends. could be useful for determining difficulty
+          deadEnds.push(JSON.parse(JSON.stringify(workBoard)));
+          return;
         } else if (cantPlay.length === 1) {
           makePlay(workBoard, moves, rownum, colnum, false, cantPlay);
           i = 0;  // reset to top of board
+          guess = {surrounding: 0}; // reset guesses
         } else {
-          console.log("possible guess found", possibleGuess);
           if (possibleGuess.surrounding >= guess.surrounding) {
             guess = possibleGuess;
           }
@@ -110,49 +112,34 @@ function levelSolveable (board) {
         i++;
       }
     }
-    // processing after it's made a full pass
-    console.log("board after a pass", workBoard);
-    console.log("moves after a pass", moves, moves.length, emptyCount);
-    console.log("guess status", guess);
-    if (moves.length === emptyCount) {
-      return true;
-    } else {
-      if (guess !== null) {
-        makePlay(workBoard, 
-          moves, 
-          guess.position[0],guess.position[1], 
-          true, guess.playType);
-        let pathBad = solveBoard();
-        if (pathBad) {
-          let {nextPlayType, nextPosition} = rollbackToLastGuess(workBoard, moves);
-          makePlay(workBoard, 
-            moves, 
-            nextPosition[0],nextPosition[1], 
-            true, nextPlayType);
-          return solveBoard();
-        }
-      }
+    // processing after it's completed all deterministic plays
+    // console.log("FINISHED A PASS");
+    if (moves.length === emptyCount) {  // Return if it's been solved
+      solutions.push(JSON.parse(JSON.stringify(workBoard)));
+      // console.log("FOUND SOLUTION");
+    } else {  // Continue to guessing if not yet solved
+      makePlay(workBoard, 
+        moves, 
+        guess.position[0],guess.position[1], 
+        true, guess.playType);
+      solveBoard(); // proceed having made a guess
+      // console.log("exiting first guess recursion");
+      
+      // go back and guess again, trying to find other possibilities
+      // clear plays, including the last guess
+      let {nextPlayType, nextPosition} = rollbackToLastGuess(workBoard, moves);
+      makePlay(workBoard, 
+        moves, 
+        nextPosition[0],nextPosition[1], 
+        true, nextPlayType);
+      solveBoard();
+      rollbackToLastGuess(workBoard, moves);
+      // console.log("exiting second guess recursion");
     }
   }
 
-  return solveBoard();
-}
-
-function rollbackToLastGuess(board, moves) {
-  let nextPosition, nextPlayType;
-  for (let i = moves.length - 1; i >= 0; i--) {
-    if (!moves[i].isGuess) {
-      board [moves[i].position[0]] [moves[i].position[1]] = 0;
-      moves.pop();
-    } else {
-      board [moves[i].position[0]] [moves[i].position[1]] = 0;
-      nextPlayType = moves[i].playType === 1 ? 2 : 1;
-      nextPosition = moves[i].position;
-      moves.pop();
-      break;
-    }
-  }
-  return {nextPlayType, nextPosition}
+  solveBoard();
+  return solutions;
 }
 
 function prepareBoard (board) {
@@ -163,7 +150,7 @@ function prepareBoard (board) {
       if(board[r][c]!== 3) {
         mappedValue = board[r][c] % 3
       } else {
-        mappedValue = -1;
+        mappedValue = 9;
       }
       workBoard [r][c] = mappedValue;
       if (workBoard[r][c] === 0) {emptyCount++}
@@ -180,103 +167,100 @@ function makePlay (board, moves, rownum, colnum, isGuess, playInfo) {
     playType = playInfo;
   }
   board[rownum][colnum] = playType;
+  let newMove = {position: [rownum, colnum], playType, isGuess};
   moves.push({position: [rownum, colnum], playType, isGuess});
+  // console.log("MAKING MOVE NUM", moves.length, "---", newMove);
 }
-
-// change board to 0,1,10 for different types
-
-// recursive contents (takes current board state)
-// did we break the rules?
-// is full? > return true
-// create list of open spaces
-//   get number of adjacent squares and their types/counts
-//   while doing, check for obviousPlays and make them
-//     update board
-//     do not add them to empty
-
-
-// need record of plays: array of {location: [R,C], play: 1 or 10, guess: Bool}
-
-
-
-//Checks
-
-
-// while not full (count )
-  // new empty candidate {}
-  // loop through from top, looking for obvious plays, remembering guess candidates
-    // check play of both
-      // if none ok
-        // roll back to last guess, change guess content (keep rolling back if already 2nd guess) and restart loop
-      // if one ok
-        // make play (update board and add to record of plays)
-        // loop
-      // if both ok
-        // if bordering tile exists, remember as guess candidate {location:[], neighbors}
-    //
 
 function checkPlay (board, row, col) {
   const size = board[0].length;
   let cantPlay = [];
-  let surrounding = [0,0,0];
   const patterns = [
-    [[-1,size,1,size],[0,-1],[0,-2], false], // left (1<col)
-    [[1,size,-1,size],[-1,0],[-2,0], false], // up (1<row)
-    [[-1,size-2,-1,size],[1,0],[2,0], false], // down (row < size-2)
-    [[-1,size,-1,size-2],[0,1],[0,2], false], // right (col < size-2)
+    // [info for position check],[first square to check],[second square]
+    [[-1,size,1,size],[0,-1],[0,-2]], // two squares to the left
+    [[1,size,-1,size],[-1,0],[-2,0]], // two squares up
+    [[-1,size-2,-1,size],[1,0],[2,0]], // two squares down
+    [[-1,size,-1,size-2],[0,1],[0,2]], // two squares to the right
 
-    [[-1,size,0,size-1],[0,-1],[0,1], true], // leftRight (0<col<size-1)
-    [[0,size-1,-1,size],[-1,0],[1,0], true], // upDown (0<row<size-1)
-    [[0,size-1,0,size-1],[-1,-1],[1,1], true], // upLeftDownRight (0<row<size-1,0<col<size-1)
-    [[0,size-1,0,size-1],[1,-1],[-1,1], true], // downLeftUpRight (0<row<size-1,0<col<size-1)
+    [[-1,size,0,size-1],[0,-1],[0,1]], // square left and right
+    [[0,size-1,-1,size],[-1,0],[1,0]], // square above and below
+    [[0,size-1,0,size-1],[-1,-1],[1,1]], // square to upper-left, lower-right
+    [[0,size-1,0,size-1],[1,-1],[-1,1]], // square to lower-left, upper-right
 
-    [[1,size,1,size],[-1,-1],[-2,-2], false], // upLeft (1<row, 1<col)
-    [[1,size,-1,size-2],[-1,1],[-2,2], false], // upRight (1<row, col < size-2)
-    [[-1,size-2,1,size],[1,-1],[2,-2], false], // downLeft (row<size-2, 1<col)
-    [[-1,size-2,-1,size-2],[1,1],[2,2], false] // downRight (row<size-2, col<size-2)
+    [[1,size,1,size],[-1,-1],[-2,-2]], // 2 squares up & left
+    [[1,size,-1,size-2],[-1,1],[-2,2]], // 2 squares up & right
+    [[-1,size-2,1,size],[1,-1],[2,-2]], // 2 squares down & left
+    [[-1,size-2,-1,size-2],[1,1],[2,2]] // 2 squares down & right
   ];
   
   for (let p of patterns) {
-    if(
+    if( // checks position to see if the check can be performed (not too close to the edge)
       p[0][0] < row &&
       row < p[0][1] &&
       p[0][2] < col &&
       col < p[0][3]
     ){
-      checkTwoSquares(
-        cantPlay, 
+      checkTwoSquares( // performs check for that specific pair
+        cantPlay, // passes array as reference for update
         board[row + p[1][0]] [col + p[1][1]],
-        board[row + p[2][0]] [col + p[2][1]],
-        surrounding,
-        p[3]  // pass whether to check surrounding squares or not
+        board[row + p[2][0]] [col + p[2][1]]
       );
     }
   } 
-  let possibleGuess;
+
+  // checks surrounding squares and creates a "smart guess"..?
+  let surrounding = surroundingCheck(board, row, col)
+  let possibleGuess = {};
   if (surrounding[1] >= surrounding[2]) {
-     possibleGuess = {surrounding: surrounding[1], playType: 1, position: [row, col]};
+    possibleGuess = {surrounding: surrounding[1], playType: 1, position: [row, col]};
   } else {
     possibleGuess = {surrounding: surrounding[2], playType: 2, position: [row, col]};
   }
+
   return {cantPlay, possibleGuess};
 }
 
-function checkTwoSquares (cantPlay, squareOne, squareTwo, surrounding, surroundingCheck) {
-  if (squareOne === squareTwo) {
+function checkTwoSquares (cantPlay, squareOne, squareTwo) {
+  let checker = [1,2];
+  if (squareOne === squareTwo && checker.includes(squareOne) ) {
     if (cantPlay.indexOf(squareOne) === -1) {
-      squareOne ? cantPlay.push(squareOne) : 0;
+      squareOne ? cantPlay.push(squareOne) : 0; // modifies the calling method's cantPlay array directly
     }
   }
-  if (surroundingCheck) {
-    console.log("hecking surrounding");
-    if (0 < squareOne && squareOne < 3) {
-      surrounding[squareOne]++;
-      console.log("found surrounding 1", surrounding[squareOne]);
-    }
-    if (0 < squareTwo && squareTwo < 3) {
-      surrounding[squareTwo]++;
-      console.log("found surrounding 2", surrounding[squareTwo]);
+}
+
+function surroundingCheck (board, rownum, colnum) {
+  // set start/end row/col so the method doesn't go off the board
+  let startRow = Math.max(rownum - 1, 0);
+  let startCol = Math.max(colnum - 1, 0);
+  let endRow = Math.min(rownum + 1, board[0].length - 1);
+  let endCol = Math.min(colnum + 1, board[0].length - 1);
+  let surroundingCheck = [0,0,0];
+  for (let r = startRow; r <= endRow; r++) {
+    for (let c = startCol; c <= endCol; c++) {
+      if (board[r][c] > 0 && board[r][c] < 3) { // only counting 1s and 2s
+        surroundingCheck[board[r][c]] ++;
+      }
     }
   }
-  return null;
+  return surroundingCheck;
+}
+
+function rollbackToLastGuess(board, moves) {
+  let nextPosition, nextPlayType;
+  for (let i = moves.length - 1; i >= 0; i--) {
+    if (!moves[i].isGuess) {
+      board [moves[i].position[0]] [moves[i].position[1]] = 0;
+      let temp = moves.pop();
+      // console.log(`Undoing: ${JSON.stringify(temp)}`);
+    } else {
+      board [moves[i].position[0]] [moves[i].position[1]] = 0;
+      nextPlayType = moves[i].playType === 1 ? 2 : 1;
+      nextPosition = moves[i].position;
+      let temp = moves.pop();
+      // console.log(`Undoing: ${JSON.stringify(temp)}`);
+      break;
+    }
+  }
+  return {nextPlayType, nextPosition}
 }
